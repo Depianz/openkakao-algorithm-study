d25f0006cdff9e349070add76efab9c1e4c25b35
Defian <iautm91@gmail.com>
2021-12-10 09:05:23 +0900

    week9 study

---
+++ src/week9/프로그래머스_올바른괄호_1번/Solution.java
@@ -0,0 +1,18 @@
+package week9.프로그래머스_올바른괄호_1번;
+
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') st.push(c);
+            else {
+                if(st.empty()) return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+}
